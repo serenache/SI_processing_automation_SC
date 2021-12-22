618763da4427b69f47f940eb577a0b6f36f32343
amb <a.brosse@gcg.co.uk>
2021-12-22 18:15:23 +0000

    Centralised way to set global variables

---
+++ general_main.py
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------
+#Module Import
+
+
+#-------------------------------------------------------------------------------
+#Import data from database
+
+#need to write some general functions to upload data from gINT database
+# onto python - to adapt from sc work on CPT
+
+#-------------------------------------------------------------------------------
+#Object classes
+
+#need to create some super classes for lab and in-situ tests
+#need to think of the structure of the stuff first
---
+++ global_constant_offshore.py
@@ -0,0 +1,6 @@
+#To be imported to set universal physical constants (global variables)
+
+unit_weight_water = 10 #kN/m3
+gravity_acceleration = 9.81 #m/s2
+atm_pressure = 101.325 #kPa
+
